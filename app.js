eea572d30f35e4e2481ec1d8fcb17865aece4e47
thiago <tchiqueti@gmail.com>
2023-07-07 17:28:07 -0300

    Melhorias de server com express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express')
+
+const app = express();
+//chamada do root '/'
+app.get('/', (req, res)=>{
+  res.send('<h1>Lista de tarefas</h1>')
+})
+
+app.get('/json', (req, res)=>{
+  res.json([{title: 'Terefa X', done: true}, {title: 'Terefa X', done: true}])
+})
+
+app.listen(3000, ()=>{
+  console.log('Server Started')
+})
